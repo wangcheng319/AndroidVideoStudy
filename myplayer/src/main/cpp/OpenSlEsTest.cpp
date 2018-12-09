b7ac48c3d40669f29879d2153185715d1e4b1db9
wangc <274281610@qq.com>
2018-12-09 20:44:06 +0800

    aac 编码

---
+++ myplayer/src/main/cpp/OpenSlEsTest.cpp
@@ -0,0 +1,29 @@
+//
+// Created by wangcheng on 2018/12/9.
+//
+
+#include "OpenSlEsTest.h"
+#include "AndroidLog.h"
+
+SLEngineItf
+
+
+OpenSlEsTest::OpenSlEsTest(string url) {
+
+    LOGE("配置=%s", url.data())
+    this->url = url;
+
+}
+
+
+
+
+void OpenSlEsTest::beginPlay() {
+    LOGE("配置=%s", url.data())
+
+
+}
+
+
+
+
